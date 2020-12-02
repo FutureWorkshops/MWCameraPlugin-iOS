dba8a267dabe2475e0d9e33e5d569f1e36da8bc7
Xavi Moll <xmollv@gmail.com>
2020-12-02 12:21:23 +0100

    Added the rest of the skeleton for the camera plugin

---
+++ MWCameraPlugin/MWCameraPlugin/QR Code Step/MWQRCodeStep.swift
@@ -0,0 +1,51 @@
+//
+//  MWQRCodeStep.swift
+//  MWCameraPlugin
+//
+//  Created by Xavi Moll on 2/12/20.
+//
+
+import Foundation
+import MobileWorkflowCore
+
+public struct MWCameraPlugin: MobileWorkflowPlugin {
+    public static var allStepsTypes: [MobileWorkflowStepType] {
+        return MWCameraStepType.allCases
+    }
+}
+
+public enum MWCameraStepType: String, MobileWorkflowStepType, CaseIterable {
+    
+    case qrCode = "qrCode"
+    
+    public var typeName: String {
+        return self.rawValue
+    }
+    
+    public var stepClass: MobileWorkflowStep.Type {
+        switch self {
+        case .qrCode: return MWCameraQRCodeStep.self
+        }
+    }
+}
+
+public class MWCameraQRCodeStep: ORKStep {
+    
+    override init(identifier: String) {
+        super.init(identifier: identifier)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func stepViewControllerClass() -> AnyClass {
+        return MWQRCodeStepViewController.self
+    }
+}
+
+extension MWCameraQRCodeStep: MobileWorkflowStep {
+    public static func build(step: StepInfo, services: MobileWorkflowServices) throws -> ORKStep {
+        return MWCameraQRCodeStep(identifier: step.data.identifier)
+    }
+}
---
+++ MWCameraPlugin/MWCameraPlugin/QR Code Step/MWQRCodeStepViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MWQRCodeStepViewController.swift
+//  MWCameraPlugin
+//
+//  Created by Xavi Moll on 2/12/20.
+//
+
+import UIKit
+import Foundation
+
+final class MWQRCodeStepViewController: UIViewController {
+
+}
