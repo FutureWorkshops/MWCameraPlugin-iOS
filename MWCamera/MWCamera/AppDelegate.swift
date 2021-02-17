072dc0c61ca5c255657d5c35bfba5de2664211b8
Jonathan Flintham <jonathan@futureworkshops.com>
2021-02-17 14:36:00 +0000

    Using latest MWCore

--- MWCamera/MWCamera/AppDelegate.swift
+++ MWCamera/MWCamera/AppDelegate.swift
@@ -5,31 +5,19 @@
 //  Copyright © Future Workshops. All rights reserved.
 //
 
-import UIKit
 import MobileWorkflowCore
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, AuthRedirector {
+class AppDelegate: UIResponder, UIApplicationDelegate, AppEventDelegator {
 
-    weak var authFlowResumer: AuthFlowResumer?
+    weak var eventDelegate: AppEventDelegate?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         return true
     }
     
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        
-        connectingSceneSession.userInfo = [SceneDelegate.SessionUserInfoKey.authRedirectHandler: self.authRedirectHandler()]
-        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return self.handleAuthRedirect(for: url)
-    }
-
-
 }
-
--- MWCamera/MWCamera/SceneDelegate.swift
+++ MWCamera/MWCamera/SceneDelegate.swift
@@ -6,52 +6,15 @@
 //  Copyright © 2020 Future Workshops. All rights reserved.
 //
 
-import UIKit
 import MobileWorkflowCore
 import MWCameraPlugin
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    enum SessionUserInfoKey {
-        static let authRedirectHandler = "authRedirectHandler"
-    }
+class SceneDelegate: MobileWorkflowSceneDelegate {
     
-    var window: UIWindow?
-    private var urlSchemeManagers: [URLSchemeManager] = []
-    private var rootViewController: MobileWorkflowRootViewController!
-    
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
+    override func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        let manager = AppConfigurationManager(
-            withPlugins: [MWCameraPlugin.self],
-            fileManager: FileManager.default,
-            authRedirectHandler: session.userInfo?[SessionUserInfoKey.authRedirectHandler] as? AuthRedirectHandler
-        )
-        let preferredConfigurations = self.preferredConfigurations(urlContexts: connectionOptions.urlContexts)
-        self.rootViewController = MobileWorkflowRootViewController(manager: manager, preferredConfigurations: preferredConfigurations)
+        self.dependencies.plugins = [MWCameraPlugin.self]
         
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = self.rootViewController
-        window.makeKeyAndVisible()
-        self.window = window
-    }
-    
-    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        guard let context = self.urlSchemeManagers.firstValidConfiguration(from: URLContexts) else { return }
-        self.rootViewController.loadAppConfiguration(context)
-    }
-}
-
-extension SceneDelegate {
-    
-    private func preferredConfigurations(urlContexts: Set<UIOpenURLContext>) -> [AppConfigurationContext] {
-        
-        var preferredConfigurations = [AppConfigurationContext]()
-        
-        if let appPath = Bundle.main.path(forResource: "app", ofType: "json") {
-            preferredConfigurations.append(.file(path: appPath, serverId: nil, workflowId: nil, sessionValues: nil))
-        }
-        
-        return preferredConfigurations
+        super.scene(scene, willConnectTo: session, options: connectionOptions)
     }
 }
