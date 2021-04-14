21b0674f56722e5bb3668c108c816c4caa4844ae
Xavi Moll <xmollv@gmail.com>
2021-04-14 17:06:50 +0200

    Moved the L10n to its own file

---
+++ MWCameraPlugin/MWCameraPlugin/L10n.swift
@@ -0,0 +1,23 @@
+//
+//  L10n.swift
+//  MWCameraPlugin
+//
+//  Created by Xavi Moll on 14/4/21.
+//
+
+import Foundation
+
+internal enum L10n {
+    private static let LocalizationTable = "Localizable"
+    private static var bundle: Bundle { Bundle(for: MWCameraQRCodeStep.self) }
+    
+    static func localisedString(key: String, fallback: String? = nil) -> String {
+        return bundle.localizedString(forKey: key, value: fallback, table: LocalizationTable)
+    }
+    
+    enum Camera {
+        static let errorTitle = L10n.localisedString(key: "alert.error.title")
+        static let errorMessage = L10n.localisedString(key: "alert.error.message")
+        static let qrLabel = L10n.localisedString(key: "qr.label")
+    }
+}
--- MWCameraPlugin/MWCameraPlugin/QR Code Step/MWQRCodeStep.swift
+++ MWCameraPlugin/MWCameraPlugin/QR Code Step/MWQRCodeStep.swift
@@ -28,18 +28,3 @@ extension MWCameraQRCodeStep: MobileWorkflowStep {
         return MWCameraQRCodeStep(identifier: stepInfo.data.identifier)
     }
 }
-
-internal enum L10n {
-    private static let LocalizationTable = "Localizable"
-    private static var bundle: Bundle { Bundle(for: MWCameraQRCodeStep.self) }
-    
-    static func localisedString(key: String, fallback: String? = nil) -> String {
-        return bundle.localizedString(forKey: key, value: fallback, table: LocalizationTable)
-    }
-    
-    enum Camera {
-        static let errorTitle = L10n.localisedString(key: "alert.error.title")
-        static let errorMessage = L10n.localisedString(key: "alert.error.message")
-        static let qrLabel = L10n.localisedString(key: "qr.label")
-    }
-}
