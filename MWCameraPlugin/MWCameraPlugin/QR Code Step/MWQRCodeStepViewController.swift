3ec1da84c2f46cf6a2390fe0ff5c91c726afa177
Jonathan Flintham <jonathan@futureworkshops.com>
2021-05-14 13:05:50 +0100

    Now using titleMode to specify title behaviour

--- MWCameraPlugin/MWCameraPlugin/QR Code Step/MWQRCodeStepViewController.swift
+++ MWCameraPlugin/MWCameraPlugin/QR Code Step/MWQRCodeStepViewController.swift
@@ -10,14 +10,13 @@ import MobileWorkflowCore
 
 public class MWQRCodeStepViewController: MWStepViewController {
     
+    public override var titleMode: StepViewControllerTitleMode { .customOrNone }
+    
     //MARK: Private properties
     private var qrCodeStep: MWCameraQRCodeStep { self.mwStep as! MWCameraQRCodeStep }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.navigationItem.title = nil
-        self.navigationItem.largeTitleDisplayMode = .never
         
         let qrScanner = _MWQRCodeStepViewController() { [weak self] codeFound in
             guard let self = self else { return }
